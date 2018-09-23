535e70a4538ef51d99df1424f225ba70075f802b
Apple <apple@FERNNNN.local>
2018-09-23 10:52:09 +0700

    home.ts

--- workshop_20180923/src/pages/home/home.ts
+++ workshop_20180923/src/pages/home/home.ts
@@ -8,9 +8,9 @@ import { detailPage } from '../detail/detail';
 })
 export class HomePage {
   items =[
-    {id: 1,name:"Living Museum" ,addess:"Bkk",Image:"assets/imgs/logo.png",url:"https://www.youtube.com/embed/r0YyR0_SG5k",video:"assets/video/olo.mp4"},
-    {id: 2,name:"Historical Park",addess:"Bkk",Image:"assets/imgs/logo.png",url:"https://www.youtube.com/embed/yyia9VaZwJs"},
-    {id: 3,name:"Tecnologly",addess:"Bkk",Image:"assets/imgs/logo.png",url:"https://www.youtube.com/embed/iDykNfEAt7Q"}];
+    {id: 1,name:"Living Museum" ,addess:"Bkk",Image:"assets/imgs/text_head01.png",url:"https://www.youtube.com/embed/r0YyR0_SG5k",video:"assets/video/olo.mp4"},
+    {id: 2,name:"Historical Park",addess:"Bkk",Image:"assets/imgs/text_head02.png",url:"https://www.youtube.com/embed/yyia9VaZwJs"},
+    {id: 3,name:"Tecnologly",addess:"Bkk",Image:"assets/imgs/text_head03.png",url:"https://www.youtube.com/embed/iDykNfEAt7Q"}];
 
   constructor(public navCtrl: NavController) {
 
