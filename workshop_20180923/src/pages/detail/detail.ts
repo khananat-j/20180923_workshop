1054dc98f9cc2eab125f925163751822bbb8a807
iMacPichetOLD32Gb <iMacPichetOLD32Gb@LivingMuseums-iMac.local>
2018-09-23 11:12:36 +0700

    detail.ts

--- workshop_20180923/src/pages/detail/detail.ts
+++ workshop_20180923/src/pages/detail/detail.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { NavController,Slides } from 'ionic-angular';
+import { DomSanitizer }from '@angular/platform-browser';
+import { NavParams }from 'ionic-angular';
 
 @Component({
   selector: 'page-detail',
   templateUrl: 'detail.html'
 })
 export class detailPage {
+item;
+url;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,public navParams: NavParams,private sanitized: DomSanitizer) {
+    this.item = navParams.get('myitem');
+    this.url = this.item.url;
+    console.log(this.item.name);
   }
-
 }
+
